355afa3c419fee6e53e0aeb5c88d9c21419e3d1f
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-19 07:13:29 +0300

    added functionality to read from a file using fs standard library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    /* Creating an iterator which will the arguments supplied. Our 
+     collector in this case is of type - Vector */
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     let query = &args[1];
@@ -8,4 +11,11 @@ fn main() {
 
     println!("Searching for {}",query);
     println!("In file {}", file_path);
+
+    /* fs::read_to_string takes the file_path, opens that file, 
+    and returns a std::io::Result<String> of the file’s contents */
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");    
 }
